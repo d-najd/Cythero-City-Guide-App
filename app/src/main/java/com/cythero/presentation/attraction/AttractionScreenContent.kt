f9a79ae975593d96c2047ebc13e6578b9e5049a9
d najd <dimitar.hristijan.najdovski@gmail.com>
2023-02-19 17:04:50 +0100

    fixed theme

--- app/src/main/java/com/cythero/presentation/attraction/AttractionScreenContent.kt
+++ app/src/main/java/com/cythero/presentation/attraction/AttractionScreenContent.kt
@@ -1,15 +1,8 @@
 package com.cythero.presentation.attraction
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.*
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
 import com.cythero.cityguideapp.ui.attraction.AttractionScreenState
 import com.cythero.presentation.attraction.components.AttractionContent
 
--- app/src/main/java/com/cythero/presentation/library/LibraryScreenContent.kt
+++ app/src/main/java/com/cythero/presentation/library/LibraryScreenContent.kt
@@ -1,19 +1,7 @@
 package com.cythero.presentation.library
 
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Sort
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
-import com.cythero.cityguideapp.R
 import com.cythero.cityguideapp.ui.library.LibraryScreenState
 import com.cythero.presentation.library.components.LibraryContent
 import com.cythero.presentation.library.components.LibraryTopAppBar
